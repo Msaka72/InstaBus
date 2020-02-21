ccb4782290ac022b9dd0acaf9dc155ea5bd79497
Anthony Doron <35699621+Msaka72@users.noreply.github.com>
2020-02-21 23:34:44 +0100

    Base Clean

--- app/src/main/java/com/example/instabus/MapStationActivity.kt
+++ app/src/main/java/com/example/instabus/MapStationActivity.kt
@@ -5,13 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 class MapStationActivity : Fragment () {
+
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+        })
+
         return inflater.inflate(R.layout.fragment_map_station, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
     }
 }
