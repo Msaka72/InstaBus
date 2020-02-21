ef531f00448ce7f1f6899797e1a4d9f63d39ac87
Anthony Doron <35699621+Msaka72@users.noreply.github.com>
2020-02-21 21:55:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/instabus/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.instabus.ui.main
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.instabus.ListStationActivity
+import com.example.instabus.MapStationActivity
+import com.example.instabus.R
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        if (position == 0) {
+            return ListStationActivity()
+        }
+        else {
+            return MapStationActivity()
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when(position){
+            0 -> return "LIST"
+            1 -> return "MAP"
+        }
+        return null
+    }
+
+    override fun getCount(): Int {
+        // Show 2 total pages.
+        return 2
+    }
+}
